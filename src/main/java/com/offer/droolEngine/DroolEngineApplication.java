3bcaf4e3228f00ad698c9a8496ecadcf8a05f3a0
Rahul Mahato <mahatorahul2901@gmail.com>
2025-05-24 13:10:46 +0530

    initial setup

---
+++ src/main/java/com/offer/droolEngine/DroolEngineApplication.java
@@ -0,0 +1,13 @@
+package com.offer.droolEngine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroolEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroolEngineApplication.class, args);
+	}
+
+}
